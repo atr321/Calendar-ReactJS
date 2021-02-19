d99ecb5e7a51721e0b60bbb4b439a06716cbfb39
atr321 <arthur.e.d@hotmail.com>
2021-02-18 22:10:49 -0300

    adding possibility to choose state and city on reminder component

--- src/application/components/Reminder.js
+++ src/application/components/Reminder.js
@@ -1,6 +1,5 @@
-
-
 import { useState } from 'react';
+import StatesCities from '../../utils/estados-cidades.json';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Radio from '@material-ui/core/Radio';
@@ -8,36 +7,37 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent'
 import InputLabel from '@material-ui/core/InputLabel';
-
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 import '../styles/Reminder.css';
 
-export default function Reminder({open, closePopup}) {
+export default function Reminder({ open, closePopup }) {
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [date, setDate] = useState("");
     const [time, setTime] = useState("");
     const [color, setColor] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
 
-    const localStorageLembretes = JSON.parse(localStorage.getItem('lembretes'));
+    
 
-    let lembretes = localStorage.getItem('lembretes') !== null ? localStorageLembretes : [];
+    const reminders = JSON.parse(localStorage.getItem('reminders')) || [];
 
     function handleSubmit(event) {
         event.preventDefault();
 
-        const lembrete = { title: title, description: description, color: color, date: date, time: time }
+        const reminder = { title: title, description: description, color: color, date: date, time: time, state:state, city:city }
 
-        lembretes.push(lembrete);
+        reminders.push(reminder);
 
         updateLocalStorage()
+        closePopup()
 
 
         /*lembretes.forEach(lembrete => {
@@ -46,7 +46,7 @@ export default function Reminder({open, closePopup}) {
     }
 
     function updateLocalStorage() {
-        localStorage.setItem('lembretes', JSON.stringify(lembretes))
+        localStorage.setItem('reminders', JSON.stringify(reminders))
     }
 
 
@@ -60,73 +60,102 @@ export default function Reminder({open, closePopup}) {
     return (
         <div id="container">
             <div>
-                
-                <Dialog onSubmit={handleSubmit} open={open} onClose={closePopup} aria-labelledby="form-dialog-title">
+
+                <Dialog open={open} onClose={closePopup} aria-labelledby="form-dialog-title">
 
                     <DialogTitle id="form-dialog-title">Create Reminder</DialogTitle>
-                    
-                        <DialogContent>
 
-                            <TextField id="standard-basic" onChange={(e) => setTitle(e.target.value)} required label="Title" />
-                            <TextField id="standard-basic" onChange={(e) => setDescription(e.target.value)} maxLength="30" label="Description" />
+                    <form onSubmit={handleSubmit}>
+
+                    <DialogContent>
+
+                        <TextField id="standard-basic" onChange={(e) => setTitle(e.target.value)} required label="Title" />
+                        <TextField id="standard-basic" onChange={(e) => setDescription(e.target.value)} maxLength="30" label="Description" />
 
 
-                        </DialogContent>
+                    </DialogContent>
 
-                        <DialogContent>
-                            <TextField
-                                onChange={(e) => setDate(e.target.value)}
-                                id="date"
-                                required
-                                type="date"
-                            />
-                            <TextField
-                                onChange={(e) => setTime(e.target.value)}
-                                id="time"
-                                required
-                                type="time"
-                            />
-                        </DialogContent>
+                    <DialogContent>
+                        <TextField
+                            onChange={(e) => setDate(e.target.value)}
+                            id="date"
+                            required
+                            type="date"
+                        />
+                        <TextField
+                            onChange={(e) => setTime(e.target.value)}
+                            id="time"
+                            required
+                            type="time"
+                        />
+                    </DialogContent>
 
-                        <DialogContent>
+                    <DialogContent>
 
                         <FormControl >
-        <InputLabel >Cidade</InputLabel>
-        <Select
-          native
-          //value={state.age}
-          onChange={handleChange}
-          inputProps={{
-            name: 'age',
-            id: 'age-native-simple',
-          }}
-        >
-          <option aria-label="None" value="" />
-          <option value={10}>Ten</option>
-          <option value={20}>Twenty</option>
-          <option value={30}>Thirty</option>
-        </Select>
-      </FormControl>
+                            <InputLabel >State</InputLabel>
+                            
+                            <Select 
+                                native
+                                value={state}
+                                onChange={event => {setState(event.target.value);setCity("")}}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'age-native-simple',
+                                }}>
+                                {StatesCities.estados.map(estate => 
+                                    <option key={estate.sigla} value={estate.nome}>{estate.nome}</option>
+                                    
+                                    )}
+                                    <option disabled></option>
+                            </Select>
+                        </FormControl>
 
-                        </DialogContent>
+                        <FormControl >
+                            {state.length > 0 && <>
+                            <InputLabel >City</InputLabel>
+                            
+                            <Select
+                                native
+                                value={city}
+                                onChange={event => setCity(event.target.value)}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'age-native-simple',
+                                }}>
+                                {StatesCities.estados.filter(currentState =>
+                                    currentState.nome === state
 
-                        <DialogContent>
-                            <RadioGroup row aria-label="gender" name="gender1" onChange={handleChange}>
-                                <FormControlLabel onChange={(e) => setColor(e.target.value)} value="blue" className="blueText" control={<Radio className="blueButton" size="small" />} label="Blue" />
-                                <FormControlLabel onChange={(e) => setColor(e.target.value)} value="red" className="redText" control={<Radio className="redButton" size="small" />} label="Red" />
-                                <FormControlLabel onChange={(e) => setColor(e.target.value)} value="green" className="greenText" control={<Radio className="greenButton" size="small" />} label="Green" />
-                                <FormControlLabel onChange={(e) => setColor(e.target.value)} value="grey" className="greyText" control={<Radio className="greyButton" size="small" />} label="Grey" />
-                            </RadioGroup>
-                        </DialogContent>
+                                )[0].cidades.map(city =>
+                                    <option key={city} value={city}>{city}</option>
+                                    
+                                )}
+                                <option disabled></option>
+                                
+
+                            </Select></>}
+                        </FormControl>
+
+                    </DialogContent>
+
+                    <DialogContent>
+                        <RadioGroup row aria-label="gender" name="gender1" onChange={handleChange}>
+                            <FormControlLabel onChange={(e) => setColor(e.target.value)} value="blue" className="blueText" control={<Radio className="blueButton" size="small" />} label="Blue" />
+                            <FormControlLabel onChange={(e) => setColor(e.target.value)} value="red" className="redText" control={<Radio className="redButton" size="small" />} label="Red" />
+                            <FormControlLabel onChange={(e) => setColor(e.target.value)} value="green" className="greenText" control={<Radio className="greenButton" size="small" />} label="Green" />
+                            <FormControlLabel onChange={(e) => setColor(e.target.value)} value="grey" className="greyText" control={<Radio className="greyButton" size="small" />} label="Grey" />
+                        </RadioGroup>
+                    </DialogContent>
                     <DialogActions>
                         <Button onClick={closePopup} color="primary">
                             Cancel
                         </Button>
-                        <Button onClick={closePopup} color="primary">
+                        <Button type="submit" color="primary">
                             Save
                         </Button>
-                     
+                        
                     </DialogActions>
+                    </form>
                 </Dialog>
             </div>
 
